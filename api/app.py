aa9be69a810bae81d8686571188c523a205465ab
Igor <igormcsouza@gmail.com>
2020-05-21 10:31:03 -0300

    fixing non start issue on production

--- api/app.py
+++ api/app.py
@@ -1,22 +1,25 @@
-from flask import Flask, request, jsonify, render_template
+import pickle
 from joblib import load
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 # Prepare models and prediction
 def make_a_prediction(phrase):
     regressor = load('/models/sklearn-logistc-regressor-model.joblib')
-    return 'Positive' if regressor.predict(vectorizer.transform([phrase])) else 'Negative'
+    vectorizer = pickle.load(open("/models/vectorizer.pickle", "rb"))
+
+    return 'Positivo' if regressor.predict(vectorizer.transform([phrase])) else 'Negativo'
 
 data=dict()
 
 @app.route('/')
-def main():
+def main(): 
     data['reviewresponse'] = 'yet'
     return render_template('index.html', data=data)
 
 @app.route('/resolve', methods=['POST'])
 def resolve():
     review = request.form.get('review')
-    data['reviewresponse'] = 'Positive'
+    data['reviewresponse'] = make_a_prediction(review)
     return render_template('index.html', data=data)
