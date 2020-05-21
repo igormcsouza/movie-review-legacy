542607162b746434b3caec6c633acd203d559eb8
Igor <igormcsouza@gmail.com>
2020-05-20 21:09:17 -0300

    add logic

--- api/app.py
+++ api/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, request, jsonify, render_template
+from joblib import load
 
 app = Flask(__name__)
 
 # Prepare models and prediction
 def make_a_prediction(phrase):
+    regressor = load('/models/sklearn-logistc-regressor-model.joblib')
     return 'Positive' if regressor.predict(vectorizer.transform([phrase])) else 'Negative'
+
+data=dict()
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    data['reviewresponse'] = 'yet'
+    return render_template('index.html', data=data)
+
+@app.route('/resolve', methods=['POST'])
+def resolve():
+    review = request.form.get('review')
+    data['reviewresponse'] = 'Positive'
+    return render_template('index.html', data=data)
