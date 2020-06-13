b5893a6a2a83532f1a44ee6922e26a3c3f60bc65
Igor <igormcsouza@gmail.com>
2020-06-13 12:26:29 -0300

    change database to mongo atlas

--- api/app.py
+++ api/app.py
@@ -1,18 +1,31 @@
-import pickle
+from flask import Flask, request, jsonify, render_template, redirect
+from flask_pymongo import PyMongo
+
 from joblib import load
+import pickle
 import random
-from flask import Flask, request, jsonify, render_template
+import os
 
 app = Flask(__name__)
 
+mongo = PyMongo(
+    app,
+    "mongodb+srv://igorsouza:{}@myapps-z1d9g.mongodb.net/movie-review?retryWrites=true&w=majority".format(os.getenv('MONGO_PASSWORD', '1234'))
+)
+
+movie_review_collection = mongo.db.movie_review
+
 # Prepare models and prediction
+
+
 def make_a_prediction(phrase):
     regressor = load('/models/sklearn-logistc-regressor-model.joblib')
     vectorizer = pickle.load(open("/models/vectorizer.pickle", "rb"))
 
     return 'pos' if regressor.predict(vectorizer.transform([phrase]))[0] else 'neg'
 
-data=dict(reviewresponse='', review='', truth='', movie_banner_path=1)
+
+data = dict(reviewresponse='', review='', truth='', movie_banner_path=1)
 
 movie_banner_list = [
     'static/img/1.png',
@@ -27,12 +40,14 @@ movie_banner_list = [
     'static/img/10.png',
 ]
 
+
 @app.route('/')
-def main(): 
+def main():
     data['reviewresponse'] = 'yet'
     data['truth'] = 'yet'
     data['movie_banner_path'] = random.choice(movie_banner_list)
     return render_template('index.html', data=data)
+
 
 @app.route('/resolve', methods=['POST'])
 def resolve():
@@ -41,13 +56,15 @@ def resolve():
 
     return render_template('index.html', data=data)
 
+
 @app.route('/store', methods=['POST'])
 def store():
     data['truth'] = request.form.get('truth')
 
-    with open('newbase.csv', 'a+') as f:
-        f.writelines("{},{},{}\n".format(
-            '"'+ data['review'] + '"', data['reviewresponse'], data['truth']
-        ))
-    
+    try:
+        movie_review_collection.insert(data)
+    except Exception as e:
+        print('[ERROR]', e)
+        return redirect('/')
+
     return render_template('index.html', data=data)
